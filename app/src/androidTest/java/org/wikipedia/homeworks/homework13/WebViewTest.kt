12165cdf5d60a1ff7573bd4be264393acdddc9a3
e.petukhova <e.petukhova@yadro.com>
2025-04-13 22:53:14 +0300

    homework19

--- app/src/androidTest/java/org/wikipedia/homeworks/homework13/WebViewTest.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework13/WebViewTest.kt
@@ -2,6 +2,8 @@ package org.wikipedia.homeworks.homework13
 
 import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
@@ -9,16 +11,19 @@ import org.wikipedia.homeworks.homework03.OnboardingScreen
 import org.wikipedia.homeworks.homework07.ExploreScreen
 import org.wikipedia.homeworks.homework07.NewsViewItem
 import org.wikipedia.homeworks.homework07.SearchCardViewItem
+import org.wikipedia.homeworks.homework19.Steps
 import org.wikipedia.main.MainActivity
 
-class WebViewTest : TestCase() {
+class WebViewTest : TestCase(Kaspresso.Builder.withForcedAllureSupport { }) {
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun webViewTest() {
         run {
-            OnboardingScreen.skipButton.click()
+            val steps = Steps(this)
+//            OnboardingScreen.skipButton.click()
+            steps.click(OnboardingScreen.skipButton)
             ExploreScreen.items.childWith<SearchCardViewItem> {
                 withDescendant { withText("Featured article") }
             }.perform {
